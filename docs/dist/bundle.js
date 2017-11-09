66f097903825966536147b405654004673291bdd
Tony Hallett <tonyhallett74@gmail.com>
2017-11-09 18:09:22 +0000

    test

--- docs/dist/bundle.js
+++ docs/dist/bundle.js
@@ -11438,7 +11438,9 @@ var Introduction = (function (_super) {
     function Introduction(props) {
         var _this = _super.call(this, props) || this;
         _this.addHistoryState = function () {
-            _this.props.history.push(_this.props.match.path, { myKey: "Some state" });
+            //this.props.history.push(this.props.match.path, { myKey: "Some state" });
+            _this.props.history.push("somepath", { myKey: "Some state" });
+            console.log(window.history.state);
         };
         console.log("In introduction ctor");
         if (props.location.state) {
