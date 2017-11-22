22e753967745a2c7643e0b12cda9c4508240e47e
Tony Hallett <tonyhallett74@gmail.com>
2017-11-22 10:36:55 +0000

    correction should have used element.props

--- react-sun-crossword/src/index.tsx
+++ react-sun-crossword/src/index.tsx
@@ -37,8 +37,8 @@ class RouteProvider extends React.Component<any, any>{
             delete route.children;
 
         }
-        if (element.routeCallback) {
-            element.routeCallback(route);
+        if (element.props.routeCallback) {
+            element.props.routeCallback(route);
         }
         RouteProvider.routes.push(route);
         return route;
