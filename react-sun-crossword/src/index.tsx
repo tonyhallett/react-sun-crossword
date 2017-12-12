f52661c79982c1146647a07c54024994193a2ff1
Tony Hallett <tonyhallett74@gmail.com>
2017-12-12 10:46:41 +0000

    removing unnecessary Radium()

--- react-sun-crossword/src/index.tsx
+++ react-sun-crossword/src/index.tsx
@@ -1216,7 +1216,7 @@ class TicTacToeApp extends React.Component<TicTacToeAppProps, undefined>{
         <VerticallyCenteredContainer backgroundColor="orange">
             <HorizontalCenter>
                 <div style={{ backgroundColor: "gray", padding: 10 }}>
-                    <RadiumDemo/>
+                    <Demo/>
                     <div style={{ display: "inline-block" }}>
                         <div style={{ marginTop: 10, marginBottom: 10 }}>
                             <ConnectedScoreboard />
